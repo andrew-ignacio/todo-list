7a901fbf8c6e42fcf6e901bc4c5c536138a8c43f
Andrew Ignácio <andrewignacio@outlook.com.br>
2024-01-29 11:17:12 -0300

    Add item to list

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,31 @@
-const input = document.querySelector('input');
-const add = document.querySelector('add');
+const input = document.querySelector('.input');
+const add = document.querySelector('.add');
 
-const list = document.querySelector('list');
+const list = document.querySelector('.list');
+
+function createListItem() {
+  const li = document.createElement('li');
+  li.classList.add('list-item');
+  return li;
+}
+
+function addTaskToList(task) {
+  const element = createListItem();
+  element.innerHTML = task;
+
+  list.appendChild(element);
+
+  input.value = '';
+}
 
 add.addEventListener('click', (event) => {
+  if (!input.value) return;
 
+  addTaskToList(input.value);
+});
+
+input.addEventListener('keypress', (event) => {
+  if (event.keyCode !== 13) return;
+  
+  addTaskToList(input.value);
 });
