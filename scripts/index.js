033a6b077ad4608c8d9a1bc0a58173f537550561
Andrew Ignácio <andrewignacio@outlook.com.br>
2024-01-29 11:00:29 -0300

    Added style

---
+++ scripts/index.js
@@ -0,0 +1,8 @@
+const input = document.querySelector('input');
+const add = document.querySelector('add');
+
+const list = document.querySelector('list');
+
+add.addEventListener('click', (event) => {
+
+});
